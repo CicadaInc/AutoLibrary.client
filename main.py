a697d5acfed20eb99c8f4b943ed8a42c1a31d200
Mark <mark.2406@yandex.ru>
2019-07-05 11:57:19 +0500

    First trial version of sending commands to the printer

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5 import uic
 import sys
 import os
 
+import ctypes
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -12,7 +14,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("AutoLibrary")
 
         self.ean = None
-        self.ids = None
+        self.bar_codes = None
 
         self.chooseFileBut.clicked.connect(self.get_chosen_file)
         self.printBut.clicked.connect(self.print_labels)
@@ -25,7 +27,7 @@ class MainWindow(QMainWindow):
         try:
             if os.path.splitext(file_path)[1] == '.txt':
                 filename = os.path.basename(file_path)
-                self.ean, self.ids = self.parse_file(filename)
+                self.ean, self.bar_codes = self.parse_file(filename)
 
                 self.fileNameLabel.setText(filename)
                 self.infoLabel.setText('Файл успешно выбран')
@@ -40,7 +42,19 @@ class MainWindow(QMainWindow):
             self.infoLabel.setText("Выбран неверный файл конфигурации")
 
     def print_labels(self):
-        pass
+        tsc_lib = ctypes.WinDLL("TSCLIB.dll")
+
+        tsc_lib.openportW("TSC Alpha-2R")
+        tsc_lib.sendcommandW("SIZE 100 mm, 63 mm")
+        tsc_lib.sendcommandW("GAP 2 mm, 0 mm")
+        tsc_lib.sendcommandW("DIRECTION 1")
+        tsc_lib.sendcommandW("CLS")
+
+        for bar_code in self.bar_codes:
+            tsc_lib.barcodeW("10", "150", self.ean, "80", "1", "0", "2", "4", bar_code)
+            tsc_lib.printlabelW("1", "1")
+
+        tsc_lib.closeport()
 
     @staticmethod
     def parse_file(filename):
